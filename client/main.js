1e35fbef3e0f0db6588e678d0fba271e267bc17e
a-mitani <akiramitani1207@gmail.com>
2016-03-12 12:06:04 +0900

    added Node Status Component

---
+++ client/main.js
@@ -0,0 +1,15 @@
+Template.nodeStatusComponent.helpers({
+//  accounts: accountsData
+  currentProvider: function(){
+    return web3.currentProvider.host;
+  },
+  isMining: function(){
+    return web3.eth.mining;
+  },
+  currentHashrate: function(){
+    return web3.eth.hashrate;
+  },
+  currentPeerCount: function(){
+    return web3.net.peerCount;
+  }
+});
