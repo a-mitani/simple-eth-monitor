baaa4bf9cca3e4271b577ad91ae2b7113c6555ed
a-mitani <akiramitani1207@gmail.com>
2016-03-14 10:32:16 +0900

    made node status compenent reactive.

---
+++ client/lib/observe_node.js
@@ -0,0 +1,39 @@
+var peerCountIntervalId = null;
+
+// 採掘状況を非同期で取得
+var getIsMining = function(){
+  web3.eth.getMining(function(e, res){
+    if(!e)
+      Session.set('isMining', res);
+  });
+};
+
+// HashRateを非同期で取得
+var getHashRate = function(){
+  web3.eth.getHashrate(function(e, res){
+    if(!e)
+      Session.set('hashRate', res);
+  });
+};
+
+// PeerCountを非同期で取得
+var getPeerCount = function(){
+  web3.net.getPeerCount(function(e, res){
+    if(!e)
+      Session.set('peerCount', res);
+  });
+};
+
+observeNode = function(){
+  Session.setDefault('isMining', false);
+  Session.setDefault('hashRate', 0);
+  Session.setDefault('peerCount', 0);
+  Meteor.clearInterval(peerCountIntervalId);
+  peerCountIntervalId = Meteor.setInterval(function() {
+    getIsMining();
+    getHashRate();
+    getPeerCount();
+  }, 1000);
+};
+
+observeNode();
